14d7a71d610730107d579785c693228221b73c55
ridz-rs <40141138+ridz-rs@users.noreply.github.com>
2019-10-21 21:52:00 -0400

    Detailed docstrings added

--- game.py
+++ game.py
@@ -1,34 +1,60 @@
+from Game_environment import GameEnvironment
+import pygame
+
+
 class Game:
     """
-    Class to represent a single game.
-    instance variables:
-    level = The difficulty level of the game. This increases as more food is eaten by the snake
-    score = The score the user has scored. This is calculated using the score
-    """
+       Class to represent a single game.
+       instance variables:
+       int level = The difficulty level of the game. This is obtained from the
+        food in the environment
+       int score = The score the user has scored.
+       environment = The game environment object in which this game is player
+       """
     def __init__(self):
         pass
 
-    def increase_level(self):
+    def set_level(self, level):
         """
-        method that increases the level of the game
+        Sets the initial difficulty level of the game. Changes the food levels
+        in the environment accordingly and changes the fps of the environment.
+        :param level: The initial level of the game.
         :return: None
         """
-    pass
-    def end_game(self):
-        """
-        method that ends the game. At the end of the game, the score is displayed
-        :return: score
-        """
+        pass
 
-
-    def get_score(self):
+    def run_game(self):
         """
-        calculates and returns the current score of the player.
-        :return: score
-        """
-
-    def increase_score(self):
-        """
-        Method that allows the Food class to increase the score of the game
+        Calls the events method on this game's environment and then calls
+        end_game_display when the game ends
         :return: None
         """
+
+    def end_game_display(self):
+        """
+        This method creates and displays a pygame window with the appropriate
+        message displayed
+        """
+
+
+def get_score(game):
+    """
+    Returns the score scored in given game. The score is calculated using the
+    level and amount eaten.
+    :param game: Game object
+    :return: int
+    """
+
+
+def get_final_score(game):
+    """
+    Implementation yet to decided.
+    :param game:
+    :return:
+    """
+
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run_game()
